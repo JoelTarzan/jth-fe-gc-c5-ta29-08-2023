8dd4d177efc2b76e70bac115d88a535d5726189a
Joel Tarzán <45524750+JoelTarzan@users.noreply.github.com>
2023-08-24 23:43:34 +0200

    Ejercicio08 añadido

---
+++ src/main/java/Ejercicio08/ViewEjercicio08.java
@@ -0,0 +1,112 @@
+package Ejercicio08;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+import java.awt.event.ActionEvent;
+
+public class ViewEjercicio08 extends JFrame {
+
+	private JPanel contentPane;
+	private String modo = "pesetas";
+	private JTextField textFieldCantidad;
+	private JTextField textFieldResultado;
+	private JButton btnConversor;
+	private JButton btnCambiarModo;
+	private JButton btnBorrar;
+
+	/**
+	 * Create the frame.
+	 */
+	public ViewEjercicio08() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 488, 300);
+		setVisible(true);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		// Cantidad a convertir
+		JLabel lblCantidad = new JLabel("Cantidad a convertir");
+		lblCantidad.setBounds(10, 40, 123, 14);
+		contentPane.add(lblCantidad);
+
+		textFieldCantidad = new JTextField();
+		textFieldCantidad.setBounds(143, 37, 86, 20);
+		contentPane.add(textFieldCantidad);
+		textFieldCantidad.setColumns(10);
+
+		// Resultado
+		JLabel lblResultado = new JLabel("Resultado");
+		lblResultado.setBounds(281, 40, 75, 14);
+		contentPane.add(lblResultado);
+
+		textFieldResultado = new JTextField();
+		textFieldResultado.setEditable(false);
+		textFieldResultado.setEnabled(false);
+		textFieldResultado.setBounds(366, 37, 86, 20);
+		contentPane.add(textFieldResultado);
+		textFieldResultado.setColumns(10);
+
+		btnConversor = new JButton("Euros a Pesetas");
+		btnConversor.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					double cantidad = Double.parseDouble(textFieldCantidad.getText());
+					DecimalFormat formato = new DecimalFormat("#.###");
+
+					if (modo.equals("euros")) {
+						textFieldResultado.setText(formato.format(cantidad / 166.386) + "");
+
+					} else {
+						textFieldResultado.setText(formato.format(cantidad * 166.386) + "");
+					}
+
+				} catch (Exception ex) {
+					JOptionPane.showMessageDialog(contentPane, "El valor introducido es incorrecto.");
+				}
+			}
+		});
+		btnConversor.setBounds(63, 85, 134, 23);
+		btnConversor.setFocusable(true);
+		contentPane.add(btnConversor);
+
+		// Cambiar modo
+		btnCambiarModo = new JButton("Cambiar");
+		btnCambiarModo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (modo == "euros") {
+					modo = "pesetas";
+					btnConversor.setText("Euros a Pesetas");
+
+				} else {
+					modo = "euros";
+					btnConversor.setText("Pesetas a Euros");
+				}
+			}
+		});
+		btnCambiarModo.setBounds(217, 85, 89, 23);
+		btnCambiarModo.setFocusable(true);
+		contentPane.add(btnCambiarModo);
+		
+		// Botón de borrar
+		btnBorrar = new JButton("Borrar");
+		btnBorrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textFieldCantidad.setText("");
+				textFieldResultado.setText("");
+			}
+		});
+		btnBorrar.setBounds(331, 85, 89, 23);
+		btnBorrar.setFocusable(true);
+		contentPane.add(btnBorrar);
+	}
+}
---
+++ src/main/java/Ejercicio08/ejercicio08App.java
@@ -0,0 +1,9 @@
+package Ejercicio08;
+
+public class ejercicio08App {
+
+	public static void main(String[] args) {
+		
+		ViewEjercicio08 view = new ViewEjercicio08();
+	}
+}
